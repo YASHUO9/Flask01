31882b39f9b948c5c200e024cc5f01ac006f6c27
YASHUO9 <jp985083@gmail.com>
2024-02-27 00:01:42 +0530

    New

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<h1>Hello, World!</h1>"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return "Welcome to the contact page!"   
+
+
+@app.route("/blog/<int:score>")
+def blog(score):
+    return "Welcome to the blog page! And the score is: " + str(score)
+
+@app.route("/calculate")
+def calculate():
+    return render_template("calculate.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
